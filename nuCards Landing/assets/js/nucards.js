fa7a6157cf9d2a89618b889ee623fe419adc997e
Austin Heywood <austinheywood@gmail.com>
2015-06-11 16:54:46 -0700

    Additional Edits & Updates to nuCards
    edits to nucards destination landing site;

---
+++ nuCards Landing/assets/js/nucards.js
@@ -0,0 +1,53 @@
+//Bootstrap tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+}); 
+
+
+// On Browser Window Resize Add/Remove Collapse Class to Main Navigation
+window.onresize = function() {
+  if (window.innerWidth >= 769) {
+    $('#navigation').removeClass('collapse');
+  } else if (window.innerWidth <= 768) {
+    $('#navigation').addClass('collapse');
+  }
+};
+
+
+// On Page Load Add/Remove Collapse Class to Main Navigation
+$(document).ready(function(e) { 
+  if (window.innerWidth >= 769) {
+    $('#navigation').removeClass('collapse');
+  } else if (window.innerWidth <= 768) {
+    $('#navigation').addClass('collapse');
+  }
+});
+
+
+// Mobile Navigation - Toggle bars / close icon
+$('#mobileNavControl').click(function() {
+  $("#mobileNavControl i").toggleClass("fa-bars");
+  $("#mobileNavControl i").toggleClass("fa-times");
+});
+
+
+// Twitter feed
+$(document).ready(function() {
+  $('#tweecool').tweecool({
+  //settings
+    username : 'nuhomepromos', 
+    limit : 1
+  });
+});
+
+
+// Backstretch Full Screen Background Images
+$("#homeCompany").backstretch("assets/img/blueHouseBackground.jpg");
+$("#nucardsPage-Bg").backstretch("assets/img/blueFamilyHouseBackground.jpg");
+
+
+
+
+
+
+
---
+++ nuCards Landing/assets/js/uploadArtwork.js
@@ -0,0 +1,22 @@
+// Minimum Purchase Required ?
+$(function() {
+  $('#minPurchaseCheck').change(function() {
+    $('#minPurchaseFormGroup').toggleClass('in')
+  });
+});
+
+
+// Valid Towards Specifics
+$(function() {
+  $('#validTowardsCheck').change(function() {
+    $('#validTowardsFormGroup').toggleClass('in')
+  });
+});
+
+
+// Contractor License
+$(function() {
+  $('#contractorLicenseCheck').change(function() {
+    $('#contractorLicenseFormGroup').toggleClass('in')
+  });
+});
